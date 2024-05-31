dc45e79770d5da87dc1dd601ecc239a8765e8e4f
laminhtam <laminhtam5523@gmail.com>
2024-05-31 21:23:47 +0700

    implement api gateway

---
+++ SocialMediaBE/api-gateway/src/main/java/vn/edu/iuh/fit/apigateway/security/RouterValidator.java
@@ -0,0 +1,26 @@
+package vn.edu.iuh.fit.apigateway.security;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouterValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/auth/signup",
+            "/auth/login",
+            "/oauth2/authorize/facebook",
+            "/oauth2/authorize/google"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
+    public Predicate<ServerHttpRequest> isRefreshToken =
+            request -> request.getURI().getPath().contains("/auth/refresh-token");
+}
