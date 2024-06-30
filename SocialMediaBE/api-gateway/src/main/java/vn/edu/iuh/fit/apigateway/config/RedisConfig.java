e30bd0551ab9be9bc3514fd4fef712629f78595d
laminhtam <laminhtam5523@gmail.com>
2024-06-30 15:40:54 +0700

    feat: connect cloud datasource

--- SocialMediaBE/api-gateway/src/main/java/vn/edu/iuh/fit/apigateway/config/RedisConfig.java
+++ SocialMediaBE/api-gateway/src/main/java/vn/edu/iuh/fit/apigateway/config/RedisConfig.java
@@ -4,21 +4,34 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @Configuration
 public class RedisConfig {
+
     @Value("${spring.data.redis.host}")
     private String host;
+
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Value("${spring.data.redis.password}")
+    private String password;
+
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    public JedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfig = new RedisStandaloneConfiguration();
+        redisStandaloneConfig.setHostName(host);
+        redisStandaloneConfig.setPort(port);
+        redisStandaloneConfig.setUsername("default");
+        redisStandaloneConfig.setPassword(password);
+
+        return new JedisConnectionFactory(redisStandaloneConfig);
     }
 
     @Bean
