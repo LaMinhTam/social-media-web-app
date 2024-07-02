8f59666fffdef5d104f02aa3982e7d2b7090aa84
laminhtam <laminhtam5523@gmail.com>
2024-07-02 14:00:35 +0700

    chores: connect between services, add nginx

--- SocialMediaBE/notification-service/src/main/java/vn/edu/iuh/fit/notificationservice/config/RedisConfig.java
+++ SocialMediaBE/notification-service/src/main/java/vn/edu/iuh/fit/notificationservice/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -13,12 +14,22 @@ import org.springframework.data.redis.core.RedisTemplate;
 public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String host;
+
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Value("${spring.data.redis.password}")
+    private String password;
+
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    public JedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfig = new RedisStandaloneConfiguration();
+        redisStandaloneConfig.setHostName(host);
+        redisStandaloneConfig.setPort(port);
+        redisStandaloneConfig.setUsername("default");
+        redisStandaloneConfig.setPassword(password);
+
+        return new JedisConnectionFactory(redisStandaloneConfig);
     }
 
     @Bean
