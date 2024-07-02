4845fdb2fab037cf65adf5740b5f666c76f640d7
laminhtam <laminhtam5523@gmail.com>
2024-07-02 14:43:03 +0700

    intialize chat service

--- SocialMediaBE/auth-service/src/main/java/vn/edu/iuh/fit/authservice/dto/TokenRefreshRequest.java
+++
@@ -1,4 +0,0 @@
-package vn.edu.iuh.fit.authservice.dto;
-
-public record TokenRefreshRequest(String refreshToken) {
-}
--- SocialMediaBE/post-service/src/main/java/vn/edu/iuh/fit/postservice/entity/neo4j/ReactionType.java
+++
@@ -1,5 +0,0 @@
-package vn.edu.iuh.fit.postservice.entity.neo4j;
-
-public enum ReactionType {
-    LIKE, LOVE, HAHA, WOW, SAD, ANGRY;
-}
